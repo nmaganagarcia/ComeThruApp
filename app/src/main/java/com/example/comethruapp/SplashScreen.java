16e6c2f1df78c871315a09c6708d89aac092f032
natymaganagarcia4 <naty.maganagarcia4@gmail.com>
2020-09-17 15:20:46 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/comethruapp/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.example.comethruapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        startActivity(new Intent(this,MainActivity.class));
+    }
+}
